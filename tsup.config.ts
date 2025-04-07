755b7ae8a8ecb7931a99f8ba7714d2b4bdda3fea
Jan T. Sott <git@idleberg.com>
2025-04-07 21:45:28 +0200

    chore: update tsconfig

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
 	bundle: true,
+	cjsInterop: true,
 	clean: true,
-	entry: ['src/index.ts'],
-	external: ['vscode'],
-	format: 'cjs',
+	entry: ["src/index.ts"],
+	external: ["vscode"],
+	format: "cjs",
 	minify: true,
-	noExternal: ['dot-prop'],
-	target: 'es2020',
-	outDir: 'lib',
+	noExternal: ["dot-prop"],
+	outDir: "lib",
+	platform: "node",
+	target: "es2020",
 });
