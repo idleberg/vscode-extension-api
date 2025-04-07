809f9ee2626bf6eb1d46dcf84a2e0e3c22db454e
Jan T. Sott <git@idleberg.com>
2025-04-07 21:50:08 +0200

    chore: use gulp to create image

---
+++ gulpfile.mjs
@@ -0,0 +1,13 @@
+import gulp from "gulp";
+import raster from "gulp-raster-update";
+import rename from "gulp-rename";
+
+// Convert SVG
+gulp.task("default", (done) => {
+	gulp
+		.src("./src/resources/logo.svg")
+		.pipe(raster())
+		.pipe(rename("logo.png"))
+		.pipe(gulp.dest("./resources"));
+	done();
+});
